3a424ac6f678f52fb69d1ac4ae694afa5f93ac68
Alexander Guerra <alex@lovelylabs.io>
2020-09-13 23:14:00 -0500

    Change App component to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 
-function App() {
-    return (
-        <div className='App'>
-            <h1>Hello, World!</h1>
-        </div>
-    )
+export default class extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <h1>Hello, World!</h1>
+                <p>This is a change that I want to see on the real web.</p>
+            </div>
+        )
+    }
 }
-
-export default App
