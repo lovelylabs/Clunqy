8f711d664cb4f1275a96539afb71623a0680d1c1
Alexander Guerra <alex@lovelylabs.io>
2020-09-14 00:50:46 -0500

    Make an api folder

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ export default class extends Component {
             <div className='App'>
                 <h1>Hello, World!</h1>
                 <p>This is a change that I want to see on the real web.</p>
+                <p>Make static character sheets</p>
+                <p>Make a simple dice roller</p>
+                <p>Dice rolling should take the standard notation.</p>
+                <p>Setup routing for the character pages</p>
+                <p>Just take things step by step</p>
+                <h2>NaviNari</h2>
+                <h2>FuNari</h2>
+                <h2>Vinny Copulious</h2>
+
+                <p>How should the dice roller work?</p>
+                <p>I don't know if I can get excited about anything right now.</p>
+                <p>I don't know why I do this to myself.</p>
             </div>
         )
     }
